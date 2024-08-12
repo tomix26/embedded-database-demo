9cb80659d5cbd131016d405e0714d56ca3d7b628
Tomáš Vaněk <tomix26@gmail.com>
2024-08-12 22:42:49 +0200

    upgrade the project to Spring Boot 3.3.2

--- src/main/java/com/github/tomix26/embedded/database/demo/domain/Person.java
+++ src/main/java/com/github/tomix26/embedded/database/demo/domain/Person.java
@@ -1,9 +1,9 @@
 package com.github.tomix26.embedded.database.demo.domain;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
 @Entity
 @Table(schema = "test", name = "person")
--- src/test/java/com/github/tomix26/embedded/database/demo/SpringDataJpaAnnotationTest.java
+++ src/test/java/com/github/tomix26/embedded/database/demo/SpringDataJpaAnnotationTest.java
@@ -3,17 +3,14 @@ package com.github.tomix26.embedded.database.demo;
 import com.github.tomix26.embedded.database.demo.domain.Person;
 import com.github.tomix26.embedded.database.demo.domain.PersonRepository;
 import io.zonky.test.db.AutoConfigureEmbeddedDatabase;
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-@RunWith(SpringRunner.class)
 @DataJpaTest
 @AutoConfigureEmbeddedDatabase
 public class SpringDataJpaAnnotationTest {
